e7778687ab76686f41d4247a7fc41ba3dc2fbda6
Ross Matheson <ross.matheson@submissiontechnology.co.uk>
2018-03-26 08:26:56 +0100

    wip on creating problems

---
+++ app/app/src/components/Problem/createProblem/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class CreateProblem extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      description: '',
+      tags: []
+    };
+  }
+
+  titleHandler = (event) => {
+    this.setState({title: event.target.value});
+  }
+
+  descriptionHandler = (event) => {
+    this.setState({description: event.target.value});
+  }
+
+  handleCreate = (e) => {
+    console.log(e)
+  }
+
+  render () {
+    return (
+      <div class="dp-form">
+        <input
+          className="dp-input"
+          value={this.state.title}
+          onChange={this.titleHandler}
+          placeholder="Give your problem a title"
+        />
+        <input
+          className="dp-input"
+          value={this.state.description}
+          onChange={this.descriptionHandler}
+          placeholder="Give your problem a description"
+        />
+        <button
+          className="dp-button is-primary is-block"
+          onClick={this.handleCreate}
+        >Create
+        </button>
+      </div>
+    )
+  }
+};
+
+export default CreateProblem;
+
--- app/app/src/containers/Feed/index.js
+++ app/app/src/containers/Feed/index.js
@@ -3,9 +3,17 @@ import shouldFetchFeed from '../../actions/feed';
 import { authUnlocked } from '../../actions/auth'
 import { connect } from 'react-redux'
 import FeedList from '../../components/FeedList';
+import CreateProblem from '../../components/Problem/createProblem'
 import Promise from 'bluebird'
 
 class Feed extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCreate: false
+    }
+  }
 
   componentDidMount () {
       Promise.all([
@@ -14,6 +22,32 @@ class Feed extends React.Component {
       ]).then(() => {
         this.props.dispatch(shouldFetchFeed())
       })
+  }
+
+  showCreate = () => {
+    this.setState({
+      showCreate: true
+    })
+    this.showHideCreate()
+  }
+
+  hideCreate = () => {
+    this.setState({
+      showCreate: false
+    })
+    this.showHideCreate()
+  }
+
+  showHideCreate = () => {
+    if (this.state.showCreate) {
+      return <div className="box">
+        <p>
+          <span className="margin-right">Create a problem</span>
+          <button className="dp-button is-inline" onClick={this.hideCreate}>Not right now</button>
+        </p>
+        <CreateProblem />
+      </div>
+    }
   }
 
   feedList () {
@@ -35,6 +69,11 @@ class Feed extends React.Component {
             panel 1
           </div>
           <div className="col-sm">
+            <div>
+              <button className="dp-button is-primary is-inline" onClick={this.showCreate}>Create problem</button>
+              <button className="dp-button is-secondary is-inline">What are you working on now?</button>
+              {this.showHideCreate()}
+            </div>
             {this.feedList()}
           </div>
           <div className="col col-sm-2">
