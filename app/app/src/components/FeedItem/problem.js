49935f95d7e2c121e6b597edf6bb66cdcc0bf275
Ross Matheson <ross.matheson@submissiontechnology.co.uk>
2018-09-07 06:58:28 +0100

    work on problem feed layout

--- app/app/src/components/FeedItem/problem.js
+++ app/app/src/components/FeedItem/problem.js
@@ -10,6 +10,49 @@ class FeedItemProblem extends React.Component {
     super()
     this.state = {
       //...
+      selected: 'pledge',
+      tabs: [
+        {
+          name: 'pledge',
+          visible: true
+        },
+        {
+          name: 'solution',
+          visible: false
+        },
+        {
+          name: 'comment',
+          visible: false
+        }
+      ]
+    }
+  }
+
+  showTab = (tabName) => {
+    this.setState({
+      tabs: this.state.tabs.map(tab => {
+        tab.visible = tab.name === tabName
+        return tab
+      }),
+      selected: tabName
+    })
+  }
+
+  renderTab () {
+    const visibleTab = this.state.tabs.find(tab => tab.visible)
+    if (! visibleTab) return 'we would retrun a default view'
+    switch (visibleTab.name) {
+      case 'pledge':
+        return (<PledgeList pledges={this.props.data.latest_pledges} />)
+      break;
+      case 'solution':
+        return (<SolutionsList solutions={this.props.data.solutions} />)
+      break;
+      case 'comment':
+        return ('have not done comment yet')
+      break;
+      default:
+        return ('could not find component')
     }
   }
 
@@ -19,22 +62,44 @@ class FeedItemProblem extends React.Component {
     return (
       <li>
         <div className="inner-header is-light">
-          <span className="title">Problem: {this.props.data.title}</span>
+          <div className="row">
+            <div className="col col-8">
+              <span className="title">Problem: {this.props.data.title}</span>
+            </div>
+            <div className="col col-4 has-text-right">
+              Worth: £{this.props.data.pledges_value}
+            </div>
+          </div>
         </div>
         <div className="inner">
           <div className="row">
-            <div className="col col-8">
-              <p>{this.props.data.description}</p>
+            <div className="col">
+              <p>{this.props.data.description} ...will limit length</p>
+              <p>{this.props.data.specification} ...will limit length</p>
+              <div className="margin-bottom-15"><TopicList topics={this.props.data.topics} /></div>
             </div>
-            <div className="col col-4">
-              <PledgeList pledges={this.props.data.latest_pledges} />
+            <div className="col">
+              {this.renderTab()}
             </div>
-
           </div>
+        </div>
+        <div className="tabFooter">{/* offset this to component */}
 
-          <div></div>
-          <div><SolutionsList solutions={this.props.data.solutions} /></div>
-          <div><TopicList topics={this.props.data.topics} /></div>
+            <ul className="is-tabbed-list is-small has-text-right">
+              <li
+                className={this.state.selected === 'pledge' ? 'is-active' : ''}
+                onClick={() => this.showTab('pledge')}
+              >pledges ({this.props.data.pledges_count})</li>
+              <li
+                className={this.state.selected === 'solution' ? 'is-active' : ''}
+                onClick={() => this.showTab('solution')}
+              >solutions ({this.props.data.solutions.length})</li>
+              <li
+                className={this.state.selected === 'comment' ? 'is-active' : ''}
+                onClick={() => this.showTab('comment')}
+              >comments ({this.props.data.total_comments})</li>
+            </ul>
+
         </div>
       </li>
     )
