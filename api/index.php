672cd3683d49e0a49279a806d29c62d3c9bc382e
John Saunders <john@yettimedia.co.uk>
2018-09-25 00:50:08 +0100

    api relay with origin auth

--- api/index.php
+++ api/index.php
@@ -1,6 +1,36 @@
 <?php
-if ( getenv( 'ENVIRONMENT' ) == 'development' ) {
-	phpinfo();
-} else {
-	echo 'no access '.getenv( 'ENVIRONMENT' );
+
+use DevPledge\Integrations\Curl\CurlRequest;
+
+require __DIR__.'/../vendor/autoload.php';
+
+require __DIR__ . '/Curl/CurlRequest.php';
+
+$method  = $_SERVER['REQUEST_METHOD'];
+$path    = $_SERVER['PATH_INFO'];
+$body    = file_get_contents( 'php://input' );
+$headers = getallheaders();
+try {
+	@$curl = new CurlRequest( 'nginx:8082' . $path );
+	@$curl->setMethod( $method );
+	@$curl->setData( $body );
+	@$curl->setHeaders( array_merge( $headers, [ 'Origin-Auth: xyz' ] ) );
+	@$resp = $curl->getResponse();
+	@$recHeaders = $curl->getReceivedHeaders();
+	@header( 'Content-Type: application/json;charset=utf-8' );
+	@header( 'Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD' );
+	@http_response_code( $curl->getHttpCode() );
+} catch ( \Exception | \TypeError $exception ) {
+
+	if ( ! isset( $resp ) ) {
+		$resp = json_encode( [ 'error' => 'call failed!' ] );
+	}
 }
+echo $resp;
+
+
+
+
+
+
+
