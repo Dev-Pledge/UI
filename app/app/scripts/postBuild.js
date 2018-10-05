1d7ad119a2d9bbb8df241534925dd3c3fb1874ab
John Saunders <john@yettimedia.co.uk>
2018-10-05 08:35:04 +0100

    make sure directory is cleaned before adding new files

--- app/app/scripts/postBuild.js
+++ app/app/scripts/postBuild.js
@@ -1,12 +1,17 @@
 const fs = require('fs-extra');
 const paths = require('../config/paths');
 
-function makeDirAndCopyPrebuild () {
-  fs.ensureDir(paths.appPostBuild)
-    .then(() => {
-      fs.copySync(paths.appBuild, paths.appPostBuild)
-      fs.remove(paths.appBuild)
-    })
+function makeDirAndCopyPrebuild() {
+    fs.remove(paths.appPostBuild+'/*').then(
+        () => {
+            fs.ensureDir(paths.appPostBuild)
+                .then(() => {
+                    fs.copySync(paths.appBuild, paths.appPostBuild)
+                    fs.remove(paths.appBuild)
+                })
+        }
+    );
+
 }
 
 makeDirAndCopyPrebuild()
